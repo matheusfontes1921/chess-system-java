ade8b63bd99955a3b04f7b13e60964c000f868be
Matheus Fontes <matheusfontes1921@gmail.com>
2023-04-06 14:07:04 -0300

    creation position class

---
+++ src/application/App.java
@@ -0,0 +1,10 @@
+package src.application;
+
+import src.Position;
+
+public class App {
+    public static void main(String[] args) {
+        Position pos1 = new Position(3,5);
+        System.out.println(pos1);
+    }
+}
